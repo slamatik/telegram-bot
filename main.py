e9bc7ad22619ae4d90b9f5086f8fc686ddf515ea
slamatik <vjaceslavsmatess@gmail.com>
2022-01-18 22:15:23 +0000

    main menu

--- main.py
+++ main.py
@@ -12,52 +12,49 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-class MyTelegramBot:
-    def __init__(self):
-        self.username = None
-        self.user = None
-        self.tickers = None
-        self.chat_id = None
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text(text='Main Menu', reply_markup=main_menu())
+    return CHOOSING_MAIN
 
-    def start(self, update: Update, context: CallbackContext):
-        update.message.reply_text(text='Main Menu', reply_markup=main_menu())
-        return CHOOSING_MAIN
 
-    def end(self, update: Update, context: CallbackContext):
-        update.callback_query.answer()
-        update.callback_query.edit_message_text(text='Bye')
-        return END
+def end(update: Update, context: CallbackContext):
+    update.callback_query.answer()
+    update.callback_query.edit_message_text(text='Bye')
+    return END
 
-    def portfolio(self, update: Update, context: CallbackContext):
-        update.callback_query.answer()
-        update.callback_query.edit_message_text(text='Portfolio Menu', reply_markup=portfolio_menu())
-        return CHOOSING_PORTFOLIO
 
-    def market(self, update: Update, context: CallbackContext):
-        update.callback_query.answer()
-        update.callback_query.edit_message_text(text='Market Menu', reply_markup=market_menu())
-        return CHOOSING_MARKET
+def portfolio(update: Update, context: CallbackContext):
+    update.callback_query.answer()
+    update.callback_query.edit_message_text(text='Portfolio Menu', reply_markup=portfolio_menu())
+    return CHOOSING_PORTFOLIO
 
-    def main(self):
-        updater = Updater(API_KEY)
-        dispatcher = updater.dispatcher
 
-        conversation_handler = ConversationHandler(
-            entry_points=[CommandHandler('start', self.start)],
-            states={
-                CHOOSING_MAIN: [
-                    CallbackQueryHandler(self.portfolio, pattern=f'^{PORTFOLIO}$'),
-                    CallbackQueryHandler(self.market, pattern=f'^{MARKET}$'),
-                ],
+def market(update: Update, context: CallbackContext):
+    update.callback_query.answer()
+    update.callback_query.edit_message_text(text='Market Menu', reply_markup=market_menu())
+    return CHOOSING_MARKET
 
-            },
-            fallbacks=[CallbackQueryHandler(self.end, pattern='End')],
-            allow_reentry=True)
 
-        dispatcher.add_handler(conversation_handler)
-        updater.start_polling()
-        updater.idle()
+def main():
+    updater = Updater(API_KEY)
+    dispatcher = updater.dispatcher
+
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            CHOOSING_MAIN: [
+                CallbackQueryHandler(portfolio, pattern=f'^{PORTFOLIO}$'),
+                CallbackQueryHandler(market, pattern=f'^{MARKET}$'),
+            ],
+
+        },
+        fallbacks=[CallbackQueryHandler(end, pattern='End')],
+        allow_reentry=True)
+
+    dispatcher.add_handler(conversation_handler)
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
-    MyTelegramBot().main()
+    main()
